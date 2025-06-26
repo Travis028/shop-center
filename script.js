300fc43551ee2daa321f0d1878b3ea82b101173f
Debra <debrawangeci@gmail.com>
2025-06-27 00:15:40 +0300

    add changes

--- script.js
+++ script.js
@@ -5,3 +5,32 @@ cartIcon.addEventListener("click",() => cart.classList.add("active"));
 cartClose.addEventListener("click",() => cart.classList.remove("active"));
 
 const addCartButtons = document.querySelectorAll(".add-cart");
+addCartButtons.forEach(button => {
+    button.addEventListener("click", event => {
+        const productBox = event.target.closet(".product-box");
+        addToCart(productBox);
+    });
+});
+
+const cartContent = document.querySelector(".cart-content");
+const addToCart = productBox => {
+    const productImgSrc = productBox.querySelector("img").src;
+    const productTitle = productBox.querySelector(".product-title").textContent;
+    const productPrice = productBox.querySelector(".price").textContent;
+
+    const cartBox = document.createElement("div");
+    cartBox.classList.add("cart-box");
+    cartBox.innerHTML = `
+    <img src="./Product Images/images/product1.jpg" class="cart-img">
+        <div class="cart-detail">
+            <h2 class="cart-product-title">${prouductTitle}</h2>
+            <span class="cart-price">${productPrice}</span>
+            <div class="cart-quantity">
+                <button id="decrement">-</button>
+                <span class="number">1</span>
+                <button id="decrement">+</button>
+            </div>
+        </div>
+        <i class="ri-delete-bin-line cart-remove"></i>
+        `;
+};
