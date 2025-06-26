adfadc8e21dc5afbe586a3b6cf7ab0f7750db078
Debra <debrawangeci@gmail.com>
2025-06-26 23:38:30 +0300

    add changes

---
+++ script.js
@@ -0,0 +1,7 @@
+const cartIcon = document.querySelector("#cart-icon");
+const cart = document.querySelector(".cart");
+const cartClose = document.querySelector("#cart-close");
+cartIcon.addEventListener("click",() => cart.classList.add("active"));
+cartClose.addEventListener("click",() => cart.classList.remove("active"));
+
+const addCartButtons = document.querySelectorAll(".add-cart");
